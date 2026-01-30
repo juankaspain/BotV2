f0ce4804fe8381515bf310976047f58ac80efd6b
Juan Carlos Garcia Arriero <juanca755@hotmail.com>
2026-01-30 06:53:37 +0100

    fix: Update shared utils init to export env_loader

--- shared/utils/__init__.py
+++ shared/utils/__init__.py
@@ -2,5 +2,6 @@
 
 from shared.utils.logging import Logger
 from shared.utils.helpers import format_data
+from shared.utils.env_loader import load_env_once, get_env_file_path, is_env_loaded
 
-__all__ = ['Logger', 'format_data']
+__all__ = ['Logger', 'format_data', 'load_env_once', 'get_env_file_path', 'is_env_loaded']
